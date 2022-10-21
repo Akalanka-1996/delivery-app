c0d1b7a1f87659d3e6f3b9ed36a38ab5ede79078
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-10-22 00:39:44 +0530

    added

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
+
+export default function App() {
+  registerNNPushToken(4476, 'm37QpgFObNzeXojUEArcmr');
+  return (
+    <View style={styles.container}>
+      <Text>kasun akalanka</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
