89bbe03ae90967add606990be7bdff7555899c3d
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-10-25 09:04:00 +0530

    category icons added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,14 +6,19 @@ import {AuthContext} from '../context/AuthContext'
 import { AntDesign } from '@expo/vector-icons'; 
 import axios from 'axios';
 import { BASE_URL } from '../config';
+import Header from '../components/Header';
+import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
 
 function HomeScreen() {
   const {userInfo, isLoading, logout } = useContext(AuthContext)
   const [company, setCompany] = useState([]);
-
+  const [category, setCategory] = useState('')
   const token = userInfo.token
   const area = userInfo.area
   
+  const addCategory = () => {
+    setCategory('ice')
+  }
 
   useEffect(() => {
     getCompany()
@@ -25,7 +30,7 @@ function HomeScreen() {
         headers: { Authorization: `Bearer ${token}` }
     };
 
-    const {data} = await axios.get(`${BASE_URL}/company/company-area/${area}`, config)
+    const {data} = await axios.get(`${BASE_URL}/company/company-area/${area}/${category}`, config)
     console.log('data', data);
 
     } catch (error) {
@@ -34,15 +39,32 @@ function HomeScreen() {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TouchableOpacity onPress={logout}>
+    <View>
+      {/* <TouchableOpacity onPress={logout}>
       <AntDesign style={styles.logout} name="logout" size={24} color="black" />
+
+      </TouchableOpacity> */}
+
+      <Header text="Home Screen" />
+      <Text>Hi {userInfo.name}</Text>
+      <Text>What are you looking for?</Text>
+      <TouchableOpacity onPress={addCategory}>
+      <MaterialIcons name="icecream" size={24} color="black" />
+
+      </TouchableOpacity>
+      <TouchableOpacity>
+      <FontAwesome5 name="hamburger" size={24} color="black" />
+
+      </TouchableOpacity>
+      <TouchableOpacity>
+      <FontAwesome5 name="pepper-hot" size={24} color="black" />
+
+      </TouchableOpacity>
+      <TouchableOpacity>
+      <MaterialIcons name="local-drink" size={24} color="black" />
 
       </TouchableOpacity>
 
-      <Text>Home Screen</Text>
-      <Text>{userInfo.name}</Text>
-      <Text>{userInfo.area}</Text>
 
     </View>
   );
