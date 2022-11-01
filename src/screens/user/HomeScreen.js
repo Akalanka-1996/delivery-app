caeb5e3a876b0d1db06affc282c6dd1bb5e083e1
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-11-01 22:14:11 +0530

    Home screen styling

--- src/screens/user/HomeScreen.js
+++ src/screens/user/HomeScreen.js
@@ -22,10 +22,10 @@ import { AntDesign } from "@expo/vector-icons";
 import axios from "axios";
 import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 import { AuthContext } from "../../context/AuthContext";
-import Header from '../../components/Header'
-import { BASE_URL } from '../../config';
+import Header from "../../components/Header";
+import { BASE_URL } from "../../config";
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   const { userInfo, isLoading, logout } = useContext(AuthContext);
   const [company, setCompany] = useState([]);
   const [category, setCategory] = useState("");
@@ -56,7 +56,11 @@ function HomeScreen({navigation}) {
     new Animated.Value(Dimensions.get("window").height)
   ).current;
   useEffect(() => {
-    Animated.timing(translateX, { toValue: 0, duration: 1000, useNativeDriver: true }).start();
+    Animated.timing(translateX, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
   });
 
   const ItemView = ({ item }) => {
@@ -80,11 +84,9 @@ function HomeScreen({navigation}) {
   };
 
   const getItem = (item) => {
-    //Function for click on an item
-    // alert("Area : " + item.area + " Value : " + item.category + "ID: " + item._id);
-    navigation.navigate('ViewCompany', {
+    navigation.navigate("ViewCompany", {
       paramKey: item._id,
-    })
+    });
   };
 
   const getCompany = async () => {
@@ -108,25 +110,32 @@ function HomeScreen({navigation}) {
     <View>
       <Header text="Home Screen" />
       <View style={styles.name}>
-      <Text>Hi {userInfo.name}</Text>
-
+        <Text style={styles.nameText}>Hi {userInfo.name}</Text>
       </View>
       <View>
         {category === "" ? (
           <>
-            <Text>What are you looking for?</Text>
+            <View style={styles.info}>
+              <Text style={styles.infoText}>What are you looking for?</Text>
+            </View>
             <View style={styles.categoryContainer}>
               <TouchableOpacity onPress={handleIce}>
-                <MaterialIcons name="icecream" size={72} color="black" />
+                <MaterialIcons name="icecream" size={86} color="black" />
+                <Text style={styles.iconText}>Ice Cream</Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={handleBakery}>
-                <FontAwesome5 name="hamburger" size={72} color="black" />
+                <FontAwesome5 name="hamburger" size={86} color="black" />
+                <Text style={styles.iconText}>Bakery</Text>
               </TouchableOpacity>
+            </View>
+            <View style={styles.categoryContainer}>
               <TouchableOpacity onPress={handleVeg}>
-                <FontAwesome5 name="pepper-hot" size={72} color="black" />
+                <FontAwesome5 name="pepper-hot" size={86} color="black" />
+                <Text style={styles.iconText}>Vegetables</Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={handleMilk}>
-                <MaterialIcons name="local-drink" size={72} color="black" />
+                <MaterialIcons name="local-drink" size={86} color="black" />
+                <Text style={styles.iconText}>Milk</Text>
               </TouchableOpacity>
             </View>
           </>
@@ -157,7 +166,8 @@ const styles = StyleSheet.create({
   categoryContainer: {
     // alignContent: 'flex'
     flexDirection: "row",
-    alignContent: "space-between",
+    justifyContent: "space-between",
+    padding: 40,
   },
   item: {
     padding: 10,
@@ -165,10 +175,28 @@ const styles = StyleSheet.create({
     height: 44,
   },
   name: {
-    marginLeft: 120,
-    fontStyle: 'bold',
-    fontSize: 24
-  }
+    marginLeft: 80,
+    fontStyle: "bold",
+    fontSize: 24,
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  info: {
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  iconText: {
+    fontSize: 16,
+    marginLeft: 15,
+    marginTop: 5,
+  },
 });
 
 export default HomeScreen;
